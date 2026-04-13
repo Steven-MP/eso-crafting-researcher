45c2d009c5eb482e51fe42a16a98bbff6c688ab0
Steven Pratt <stevenprattdev@proton.me>
2026-04-13 19:04:25 +0930

    Preconnect for fonts

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -20,7 +20,22 @@ export const Route = createRootRoute({
 			{ name: "theme-color", content: "#18181b" },
 			{ title: "ESO Crafting Researcher" },
 		],
-		links: [{ rel: "stylesheet", href: appCss }],
+		links: [
+			{
+				rel: "preconnect",
+				href: "https://fonts.googleapis.com",
+			},
+			{
+				rel: "preconnect",
+				href: "https://fonts.gstatic.com",
+				crossOrigin: "anonymous",
+			},
+			{
+				rel: "stylesheet",
+				href: "https://fonts.googleapis.com/css2?family=Federo&family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap",
+			},
+			{ rel: "stylesheet", href: appCss },
+		],
 	}),
 
 	shellComponent: RootDocument,
